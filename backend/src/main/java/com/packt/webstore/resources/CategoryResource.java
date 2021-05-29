d14b137b7c908f520cb66511bbb13ffd644bf0de
FFTeixeira <fernandoft76@hotmail.com>
2021-05-29 20:04:13 -0300

    Category resource

---
+++ backend/src/main/java/com/packt/webstore/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package com.packt.webstore.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.packt.webstore.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		
+		List<Category> list = new ArrayList<>();
+		
+		list.add(new Category(1L, "Tablet"));
+		list.add(new Category(2L, "Smartphone"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
