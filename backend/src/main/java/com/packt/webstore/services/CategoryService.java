ce5fd1c307ad2c96b85b0d8c54335bdee3de2726
FFTeixeira <fernandoft76@hotmail.com>
2021-05-29 21:15:07 -0300

    Database seeding

--- backend/src/main/java/com/packt/webstore/services/CategoryService.java
+++ backend/src/main/java/com/packt/webstore/services/CategoryService.java
@@ -2,23 +2,22 @@ package com.packt.webstore.services;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.packt.webstore.entities.Category;
 import com.packt.webstore.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
-	@Transactional()
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
-		
+
 		return repository.findAll();
 	}
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Smartphone');
+INSERT INTO tb_category (name) VALUES ('Laptop');
+INSERT INTO tb_category (name) VALUES ('Tablet');
+
