07621a277784a4937b646482f4a25dfc1adb442c
FFTeixeira <fernandoft76@hotmail.com>
2021-05-29 21:37:19 -0300

    DTO

--- backend/src/main/java/com/packt/webstore/services/CategoryService.java
+++ backend/src/main/java/com/packt/webstore/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.packt.webstore.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.packt.webstore.dto.CategoryDTO;
 import com.packt.webstore.entities.Category;
 import com.packt.webstore.repositories.CategoryRepository;
 
@@ -16,8 +18,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
+	public List<CategoryDTO> findAll() {
 
-		return repository.findAll();
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
+
 	}
 }
